438e0f46f95c24d631516b5a7c58b91a61adf418
francokohn <86165038+francokohn@users.noreply.github.com>
2021-08-12 21:18:21 -0300

    Update urls.py

--- src/MiSitio/urls.py
+++ src/MiSitio/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from .views import inicio
+from .view import ini
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inicio/', inicio),
-
+    path('', ini),
     path('Amigos/', include('apps.amigos.urls'))
 
 
