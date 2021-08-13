c5b2e29e63e188fd88929787893c4291fd463170
Lucas Ibañez <lucas.ibanez@gfe.frre.utn.edu.ar>
2021-08-12 21:24:13 -0300

    login

--- src/MiSitio/urls.py
+++ src/MiSitio/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import inicio
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('inicio/', inicio),
+    path('inicio/', views.inicio),
+    path('login/', views.login),
 
     path('Amigos/', include('apps.amigos.urls'))
 
--- src/MiSitio/views.py
+++ src/MiSitio/views.py
@@ -13,3 +13,11 @@ def inicio(request):
 		"lista": lista_alumnos
 	}
 	return render(request,template_name, ctx )
+
+def login(request):
+	template_name="login.html"
+
+	ctx={
+
+	}
+	return render(request,template_name, ctx )
